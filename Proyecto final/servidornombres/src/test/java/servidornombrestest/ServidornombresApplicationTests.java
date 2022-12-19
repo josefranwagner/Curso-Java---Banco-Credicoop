2c680d88a6ff271adb651dc680088873722ac28c
josefranwagner <63469178+josefranwagner@users.noreply.github.com>
2022-12-19 20:31:02 -0300

    Add files via upload

---
+++ Proyecto final/servidornombres/src/test/java/servidornombrestest/ServidornombresApplicationTests.java
@@ -0,0 +1,13 @@
+package servidornombrestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidornombresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
